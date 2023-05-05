c780b8703cd0c62532b45d5cc722f10c31f28b89
Gus700 <87691481+Gus700@users.noreply.github.com>
2023-05-04 23:28:44 -0700

    Starting to build order generation after completion

--- scripts/craftingArea.gd
+++ scripts/craftingArea.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 var currentOrderRecipe: Array[String]
 var userSelectedIngr: Array[String]
 var itemScene
+signal request_updated_order
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,15 +22,15 @@ func _on_order_area_update_recipe(recipe) -> void:
 # called when user hits the completion bell, check if the ingredients in the crafting area are
 # correct acording to the current order recipe
 func _on_completion_bell_pressed() -> void:
-	print("order complete signal recieved, check for correct ingredients")
+	print("recieved order complete signal in crafting area")
+	emit_signal("request_updated_order")
 
 # recieves global signal of ingredient button with the name of the selected ingredient
 func ingredient_selected (ingredient_name, ingredient_img) -> void:
 	print("recieved ingredient selection: ", ingredient_name)
-	var newCI = itemScene.instantiate() #create instance of Texture node
-	newCI.texture = ingredient_img
-	add_child(newCI) #Add it as a child of this node.
+	var itemSceneInst = itemScene.instantiate() #create instance of Texture node
+	itemSceneInst.texture = ingredient_img
+	add_child(itemSceneInst) #Add it as a child of this node.
 	userSelectedIngr.append(ingredient_name)#append ingredient name to userSelectedIngr Array
-	print("current crafting area: ", userSelectedIngr)
 	
 
--- scripts/orderArea.gd
+++ scripts/orderArea.gd
@@ -4,11 +4,13 @@ extends HBoxContainer
 
 var requiredOrderNum: int
 var orderNum: int
+var cardScene
 
 signal update_recipe(recipe: Array[String])
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	cardScene = preload("res://scenes/OrderCard.tscn")
 	for orderCard in get_children():
 		# set random order type index in the resource array
 		var randOrderNum: int = randi_range (0, orderTypes.size()-1)
@@ -23,3 +25,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+#recieves message from crafting area that next order info is needed
+func _on_crafting_area_request_updated_order() -> void:
+	get_child(0).queue_free() #free the current order
+	emit_signal("update_recipe", get_child(0).recipe) #send info of new current order
+	var cardSceneInst = cardScene.instantiate() #make a new card order
+	var randOrderNum: int = randi_range (0, orderTypes.size()-1)
+	cardSceneInst.get_node("OrderImage").texture = orderTypes[randOrderNum].mainTexture
+	cardSceneInst.recipe = orderTypes[randOrderNum].ingredients
+	add_child(cardSceneInst)
+	pass # Replace with function body.
