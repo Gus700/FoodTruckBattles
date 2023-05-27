b827ca8134ba3110bee6d6f4a77a474590554a9c
Gus700 <87691481+Gus700@users.noreply.github.com>
2023-05-26 18:44:49 -0700

    adding initial lore scene, cleaned up some of folders

---
+++ scripts/loreScene.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@export var img: Texture2D
+@onready var bgImgNode = $backgroundImage
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	setBgImg(img)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func setBgImg(img) -> void:
+	if img != null:
+		bgImgNode.texture = img
