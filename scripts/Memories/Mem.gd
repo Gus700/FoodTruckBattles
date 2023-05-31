f258519767ddd1817d92b683e0f66d752adc85c0
Stephanie R.S <StephStyffe@gmail.com>
2023-05-30 20:22:36 -0700

    UI adjs and Fun effects
    Added lore text to the memories, Made the trucks dance in the credits, fixed the stupid fucking scaling issues with the main menu buttons.

--- scripts/Memories/Mem.gd
+++ scripts/Memories/Mem.gd
@@ -2,10 +2,11 @@ extends TextureRect
 
 @export var img : String = "img"
 @export var big_img : String = "img"
+@export var lore_text : String = "lore"
 
 
-signal showbig(big_img)
+signal showbig(big_img,lore_text)
 
 func _on_button_pressed():
 	self.texture = load("res://assets/loreScenes/photos/"+ img +"_unlocked.png")
-	emit_signal("showbig",big_img)
+	emit_signal("showbig",big_img,lore_text)
