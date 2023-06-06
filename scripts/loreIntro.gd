4a53feef1b0c87c086f635149f9518a5bf1c13a0
Gus700 <87691481+Gus700@users.noreply.github.com>
2023-06-05 22:08:39 -0700

    button functionality to intro lore scenes

--- scripts/loreIntro.gd
+++ scripts/loreIntro.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var scns = $Scns
 @onready var numScns = scns.get_child_count()
 var currScnIndx :int = 0
-var lastScnIndx :int
+var prevScnIndx :int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,9 +17,20 @@ func _process(delta: float) -> void:
 #______________________________signal functions_________________________________
 
 func _on_next_button_pressed() -> void:
-	lastScnIndx = currScnIndx
-	currScnIndx += 1
-	currScnIndx = fposmod(currScnIndx, numScns)
-	print(currScnIndx)
-	scns.get_child(currScnIndx).show()
-	scns.get_child(lastScnIndx).hide()
+	if currScnIndx != numScns-1:
+		prevScnIndx = currScnIndx
+		currScnIndx += 1
+		currScnIndx = fposmod(currScnIndx, numScns)
+		print(currScnIndx)
+		scns.get_child(currScnIndx).show()
+		scns.get_child(prevScnIndx).hide()
+
+
+func _on_back_button_pressed() -> void:
+	if currScnIndx != 0:
+		prevScnIndx = currScnIndx
+		currScnIndx -= 1
+		currScnIndx = fposmod(currScnIndx, numScns)
+		print(currScnIndx)
+		scns.get_child(currScnIndx).show()
+		scns.get_child(prevScnIndx).hide()
