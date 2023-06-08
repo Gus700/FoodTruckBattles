8e09bb9c1f2931ff7e9be59f190e511deffcb001
Gus700 <87691481+Gus700@users.noreply.github.com>
2023-06-07 21:17:49 -0700

    logic for individual truck intro and lore complete

--- scenes/truckLore.gd
+++ scenes/truckLore.gd
@@ -2,6 +2,9 @@ extends Control
 
 @export var textImgs: Array[Texture2D]
 @onready var textBubbleNode = $textBubble
+@onready var changeSceneB = $TextureButton
+@onready var nextTextB = $NextTextButton
+@onready var prevTextB = $PrevTextButton
 var textBubIndex = 0
 
 func _ready() -> void:
@@ -10,9 +13,28 @@ func _ready() -> void:
 func setTextBubImg() -> void:
 	textBubbleNode.texture = textImgs[textBubIndex]
 
+# if the last text box is being shown then hide next button and show change scene button
+func checkIfLastText() -> void:
+	if textBubIndex == textImgs.size() - 2:
+		nextTextB.hide()
+		changeSceneB.show()
+	else:
+		changeSceneB.hide()
+		nextTextB.show()
+
 #___________________________signal function____________________________________
 
-func _on_texture_button_pressed() -> void:
+func _on_next_text_button_pressed() -> void:
+	print(textBubIndex)
+	checkIfLastText()
 	if textBubIndex < textImgs.size() - 1:
 		textBubIndex += 1
+		setTextBubImg()
+
+
+func _on_prev_text_button_pressed() -> void:
+	checkIfLastText()
+	print(textBubIndex)
+	if textBubIndex > 0:
+		textBubIndex -= 1
 		setTextBubImg()
--- scripts/loreScene.gd
+++ scripts/loreScene.gd
@@ -2,7 +2,11 @@ extends SceneScr
 
 var currentFoodTruck: String = GloData.getCurrLvlName()
 
+func _ready() -> void:
+	get_node(currentFoodTruck).show()
+
 #_________________________signal functions______________________________________
 
 func _on_texture_button_get_dest(dest) -> void:
 	emit_signal("scene_changed", dest)
+
